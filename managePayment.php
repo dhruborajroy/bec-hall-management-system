e2d91c49027daf724f07dcda006a688846ec48a2
Dhrubo Raj Roy <Dhruborajroy3@gmail.com>
2022-08-13 00:30:15 +0600

    payment page changed

--- managePayment.php
+++ managePayment.php
@@ -107,10 +107,22 @@
                             </td>
                         </tr> -->
                         <tr>
-                           <th scope="row"><input type="checkbox" value="2230"  id="checkbox" name="amount" onchange="get_total(this.value)"></th>
+                           <th scope="row"><input type="checkbox" value="1"  id="checkbox_1" name="amount" onchange="get_total(this.value)"></th>
                            <td>February - 22</td>
                            <td >
-                           <input type="hidden" value="2230" class="amount"> 
+                           <input type="hidden" value="2230" class="amount" id="amount_1"> 
+                           2230
+                           </td>
+                           
+                            <td>
+                                <button  type="button" class="btn-fill-lmd radius-30 text-light shadow-dodger-blue bg-red">Unpaid</button>
+                            </td>
+                        </tr>
+                        <tr>
+                           <th scope="row"><input type="checkbox" value="2"  id="checkbox_2" name="amount" onchange="get_total(this.value)"></th>
+                           <td>February - 22</td>
+                           <td >
+                           <input type="hidden" value="2230" class="amount" id="amount_2"> 
                            2230
                            </td>
                            
@@ -121,79 +133,6 @@
                      </tbody>
                   </table>
                   <hr>
-                  <!-- <div class="col-xl-12 col-lg-12 col-12 ">
-                     <center>
-                         <p class="header_payment">Monthly Payment</p>
-                     </center>
-                     </div>
-                     <div class="col-xl-12 col-lg-12 col-12 form-group row" id="wrap">
-                     <div class="col-xl-4 col-lg-4 col-12 form-group" id="my_box">
-                         <label>Month *</label>
-                         <select class="form-control disable_class" id="select_box_1"
-                             onchange="getMonthlyData('1')" name="month_id[]" required>
-                             <option value="">Please Select Month *</option>
-                             <?php
-                        $month_id=0;
-                        $res=mysqli_query($con,"SELECT * FROM `month` where status='1'");
-                        while($row=mysqli_fetch_assoc($res)){
-                            if($row['id']==$month_id){
-                                echo "<option selected='selected' value=".$row['id'].">".$row['name']."</option>";
-                            }else{
-                                echo "<option value=".$row['id'].">".$row['name']."</option>";
-                            }                                                        
-                        }
-                        ?>
-                         </select>
-                     </div>
-                     <div class="col-xl-4 col-lg-4 col-12 form-group">
-                         <label>Due amount</label>
-                         <input type="number" value="0" name="month_amount[]" id="number_1"
-                             class="form-control amount" readonly>
-                     </div>
-                     <input type="hidden" id="box_count" value="1">
-                     <input type="hidden" id="total_amount" value="0">
-                     <input style="margin-top: 20px;margin-bottom: 15px;" type="button" id="submit"
-                         value="Add More" class="btn-fill-lg font-normal text-light gradient-pastel-green"
-                         onclick="add_more()">
-                     </div>
-                     <hr>
-                     <div class="col-xl-12 col-lg-12 col-12 ">
-                     <center>
-                         <p class="header_payment">Fees</p>
-                     </center>
-                     </div>
-                     <div class="col-xl-12 col-lg-12 col-12 form-group row" id="wrap_fees">
-                     <div class="col-xl-4 col-lg-4 col-12 form-group" id="my_box_fees">
-                         <input type="hidden" id="fees_count" value="1">
-                         <label>Fees *</label>
-                         <select class="form-control form-control-lg" id="fee_select_box_1"
-                             onchange="getFeesData(1)" name="fees_id[]" required>
-                             <option selected readonly>Please Select fees *</option>
-                             <?php
-                        $fees_id=0;
-                        $res=mysqli_query($con,"SELECT * FROM `fees` where status='1'");
-                        while($row=mysqli_fetch_assoc($res)){
-                            if($row['id']==$fees_id){
-                                echo "<option selected='selected' value=".$row['id'].">".$row['name']."</option>";
-                            }else{
-                                echo "<option value=".$row['id'].">".$row['name']."</option>";
-                            }                                                        
-                        }
-                        ?>
-                         </select>
-                     </div>
-                     <input type="hidden" id="fee_total" value="0">
-                     <input type="hidden" id="fees_box_count" value="1">
-                     <div class="col-xl-4 col-lg-4 col-12 form-group">
-                         <label>Due amount</label>
-                         <input type="number" value="0" class="form-control fee_amount" id="fee_number_1"
-                             name="fees_amount[]" readonly>
-                     </div>
-                     <input type="hidden" id="box_count" value="1">
-                     <input style="margin-top: 20px;margin-bottom: 15px;" type="button" id="fees_submit"
-                         value="Add More" class="btn-fill-lg font-normal text-light gradient-pastel-green"
-                         onclick="add_more_fees()">
-                     </div> -->
                   <div class="row">
                      <div class="col-xl-4 col-lg-8 col-4 form-group">
                      </div>
@@ -213,12 +152,6 @@
                            Payment
                            </button>
                         </div>
-                        <!-- <div class="col-xl-2 col-lg-2 col-12 form-group">
-                           <button type="button" class="btn-fill-lmd radius-4 text-light bg-violet-blue"
-                              onclick="reload()">
-                           Reload
-                           </button>
-                        </div> -->
                      </div>
                   </div>
                </div>
@@ -231,167 +164,19 @@
 <?php include("footer.php")?>
 
 <script>
-$(document).ready(function() {
-});
-   // Finding Total Amount
-function get_total(value) {
-    $(document).on('change', '#checkbox', function() {
-        if(this.checked) {
-            // alert(value);
-            var total = 0;
-            var amount = document.getElementsByClassName("amount");
-            for (let i = 0; i < amount.length; i++) {
-            	var total = total + parseInt(amount[i].value);
-            }
-        }
-    });
-    // alert(value);
-	// var total = 0;
-	// var amount = document.getElementsByClassName("amount");
-	// for (let i = 0; i < amount.length; i++) {
-	// 	var total = total + parseInt(amount[i].value);
-	// }
-    // console.log(total); 
-    // var grant_total=total;
-	// document.getElementById("grant_total").value = grant_total;
+function get_total(id) {
+    if(document.getElementById("checkbox_"+id).checked==true){
+        jQuery('#amount_'+id).addClass('active_amount');
+    }else if(document.getElementById("checkbox_"+id).checked==false){
+        jQuery('#amount_'+id).removeClass('active_amount');
+    }
+	var total = 0;
+	var amount = document.getElementsByClassName("active_amount");
+	for (let i = 0; i < amount.length; i++) {
+		var total = total + parseInt(amount[i].value);
+	}
+    console.log(total);
+    var grant_total=total;
+	document.getElementById("grant_total").value = grant_total;
 }
-
-</script>
-<script>
-    /*
-   // jQuery("#submit").hide();
-   
-   // Retriving  Fee Amount
-   function getFeesData(id) {
-       var fee_id = jQuery("#fee_select_box_" + id).val();
-       jQuery.ajax({
-           type: "post",
-           url: "./requests/getMonthlyFee.php",
-           data: "fee_id=" + fee_id,
-           success: function(result) {
-               result = result.trim()
-               // alert(result);
-               jQuery("#fee_number_" + id).val(result);
-               get_total();
-               // jQuery("#submit").show();
-           }
-       });
-   }
-   // Retriving Monthly Due Amount
-   function getMonthlyData(id) {
-       var month_id = jQuery("#select_box_" + id).val();
-       jQuery.ajax({
-           type: "post",
-           url: "./requests/getMonthlyFee.php",
-           data: "month_id=" + month_id,
-           success: function(result) {
-               // alert(result);
-               result = result.trim()
-               jQuery("#number_" + id).val(result);
-               get_total();
-               // jQuery("#submit").show();
-           }
-       });
-   }
-   // Adding more Monthly data
-   function add_more() {
-       var option_value = '<?php
-      $month_id=0;
-      $res=mysqli_query($con,"SELECT * FROM `month` where status='1'");
-      while($row=mysqli_fetch_assoc($res)){
-          if($row['id']==$month_id){
-              echo "<option selected='selected' value=".$row['id'].">".$row['name']."</option>";
-          }else{
-              echo "<option value=".$row['id'].">".$row['name']."</option>";
-          }                                                        
-      }
-      ?>';
-       var box_count = jQuery("#box_count").val();
-       // jQuery("#submit").hide();
-       box_count++;
-       // alert(box_count);
-       jQuery("#box_count").val(box_count);
-       jQuery("#wrap").append('<div class="col-xl-12 col-lg-12 col-12 form-group row" id="box_loop_' + box_count +
-           '"><div class="col-xl-6 col-lg-6 col-12 form-group"><label>Month *</label><select required  name="month_id[]" id="select_box_' +
-           box_count + '" onchange="getMonthlyData(' + box_count +
-           ')" class="form-control disable_class"><option value="" selected readonly>Please Select Month *</option>' +
-           option_value +
-           '</select></div><div class="col-xl-4 col-lg-4 col-12 form-group"><label>Due amount</label><input type="number"  name="month_amount[]" id="number_' +
-           box_count +
-           '" value="0" class="form-control amount" readonly></div><div class="col-xl-2 col-lg-2 col-2 form-group"><input style="margin-top: 20px;" type="button" name="submit" id="submit" value="Remove" class="btn-fill-lmd text-light radius-4 bg-gradient-gplus"  onclick=remove_more("' +
-           box_count + '")></div></div>'
-       );
-       get_total();
-   }
-   
-   
-   function remove_more(box_count) {
-       jQuery("#box_loop_" + box_count).remove();
-       var box_count = jQuery("#box_count").val();
-       box_count--;
-       jQuery("#box_count").val(box_count);
-       get_total();
-   }
-   
-   // Fee Datas
-   
-   
-   function add_more_fees() {
-       var option_value = '<?php
-      $fees_id=0;
-      $res=mysqli_query($con,"SELECT * FROM `fees` where status='1'");
-      while($row=mysqli_fetch_assoc($res)){
-          if($row['id']==$fees_id){
-              echo "<option selected='selected' value=".$row['id'].">".$row['name']."</option>";
-          }else{
-              echo "<option value=".$row['id'].">".$row['name']."</option>";
-          }                                                        
-      }
-      ?>';
-       var fees_count = jQuery("#fees_count").val();
-       fees_count++;
-       jQuery("#fees_count").val(fees_count);
-       jQuery("#wrap_fees").append('<div class="col-xl-12 col-lg-12 col-12 form-group row" id="fees_loop_' +
-           fees_count +
-           '"><div class="col-xl-6 col-lg-6 col-12 form-group"><label>Month *</label><select required name="fees_id[]" class="form-control" id="fee_select_box_' +
-           fees_count + '" onclick="getFeesData(' + fees_count +
-           ')"><option value="" selected readonly>Please Select Month *</option>' + option_value +
-           '</select></div><div class="col-xl-4 col-lg-4 col-12 form-group"><label>Due amount</label><input name="fees_amount[]" type="number" value="0" id="fee_number_' +
-           fees_count +
-           '" class="form-control fee_amount" readonly></div><div class="col-xl-2 col-lg-2 col-2 form-group"><input style="margin-top: 20px;" type="button" name="submit" id="submit" value="Remove" class="btn-fill-lmd text-light radius-4 bg-gradient-gplus" onclick=remove_more_fees("' +
-           fees_count + '")></div></div>'
-       );
-       get_total();
-   }
-   
-   
-   function remove_more_fees(fees_count) {
-       jQuery("#fees_loop_" + fees_count).remove();
-       var fees_count = jQuery("#fees_count").val();
-       fees_count--;
-       jQuery("#fees_count").val(fees_count);
-   }
-   
-   
-   // Finding Total Amount
-//    function get_total() {
-//        var total = 0;
-//        var amount = document.getElementsByClassName("amount");
-//        for (let i = 0; i < amount.length; i++) {
-//            var total = total + parseInt(amount[i].value);
-//        }
-//        var fee_total = $("#fee_total").val();
-//        var fee_amount = document.getElementsByClassName("fee_amount");
-//        for (let i = 0; i < fee_amount.length; i++) {
-//            var fee_total = parseInt(fee_total) + parseInt(fee_amount[i].value);
-//        }
-//        var grant_total = total + fee_total;
-//        document.getElementById("grant_total").value = grant_total;
-//    }
-   
-   // Reloading The page to start from the begineeing
-   function reload() {
-       location.reload();
-   }
-   */
 </script>
