860b8ca5861323bdac47aa96e269b6ec0ff6e110
Dhrubo Raj Roy <Dhruborajroy3@gmail.com>
2022-10-11 18:40:32 +0600

    Modified

--- inc/constant.inc.php
+++ inc/constant.inc.php
@@ -8,20 +8,14 @@ define('STORE_PASSWORD',"thewe630883975551e@ssl");
 define('STUDENT_IMAGE',FRONT_SITE_PATH."media/users/");
 define('UPLOAD_STUDENT_IMAGE',$_SERVER['DOCUMENT_ROOT']."/admission/media/users/");
 // Bkash Test Constants
-// define("APP_KEY","4f6o0cjiki2rfm34kfdadl1eqq");
-// define("APP_SECRET","2is7hdktrekvrbljjh44ll3d9l1dtjo4pasmjvs5vl5qr3fug4b");
-// define("USERNAME","sandboxTokenizedUser02");
-// define("PASSWORD","sandboxTokenizedUser02@12345");
-// define("BASE_URL",'https://tokenized.sandbox.bka.sh/v1.2.0-beta');
-// define("FORM_AMOUNT",100);
+define("APP_KEY","4f6o0cjiki2rfm34kfdadl1eqq");
+define("APP_SECRET","2is7hdktrekvrbljjh44ll3d9l1dtjo4pasmjvs5vl5qr3fug4b");
+define("USERNAME","sandboxTokenizedUser02");
+define("PASSWORD","sandboxTokenizedUser02@12345");
+define("BASE_URL",'https://tokenized.sandbox.bka.sh/v1.2.0-beta');
+define("FORM_AMOUNT",100);
 define("SERVICE_CHARGE",.24);
 //Bkash live credentials
-define("APP_KEY","nAjRxg1lc6eEMdmwkTHyJxyztc");
-define("APP_SECRET","AabfRBCBxUxYKGNVpenNEBcfptxjVfPIDGGxlHyXbNPLliopt9oK");
-define("USERNAME","01753335815");
-define("PASSWORD","{3KOw3H@wtK");
-define("BASE_URL",'https://tokenized.pay.bka.sh/v1.2.0-beta');
-define("FORM_AMOUNT",1);
 
 $curStr=$_SERVER['REQUEST_URI'];
 $curArr=explode('/',$curStr);
